b5694ee3a6284307d8fb4e6d18f1dee06e42ef29
Georg Osang <geoo89@gmail.com>
2021-08-02 13:57:44 +0200

    commandline arguments for main (rather than hard-coded strings)

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import json
+import argparse
 
 from rapidpro_abtest_creator import RapidProABTestCreator
 from sheets import CSVMasterSheetParser, GoogleMasterSheetParser
 import logging
-logging.basicConfig(filename='main.log', level=logging.WARNING, filemode='w')
-
-SPREADSHEET_ID = '1FLDxTPNvnWPLgt1lUuCLt7Nsx9UtZoBkYihtvsGnntI'  # Sample
-# SPREADSHEET_ID = '1KPakZyyuyHoRO5GCdyde-vOvKq2155pTl-VZKKIKcXI'  # Parenting
-MASTER_CSV = "testdata/master_sheet.csv"
-JSON_FILENAME = "testdata/Linear_OneNodePerAction.json"
-# JSON_FILENAME = "data/plh-international-flavour.json"
-OUTPUT_FILENAME = "out.json"
 
 def main():
-    # parser = GoogleMasterSheetParser(SPREADSHEET_ID)
-    parser = CSVMasterSheetParser(MASTER_CSV)
-    flow_edit_sheets = parser.get_flow_edit_sheets()
-    rpx = RapidProABTestCreator(JSON_FILENAME)
+    parser = argparse.ArgumentParser(description='Apply FlowEdits/ABTests to a RapidPro JSON file.')
+    parser.add_argument('input', help='RapidPro JSON file defining the input RapidPro flows.')
+    parser.add_argument('output', help='RapidPro JSON file to write the output to.')
+    parser.add_argument('master_sheet', help='Master sheet referencing FlowEdits/ABTests. Either a csv file or a Google Sheet ID.')
+    parser.add_argument('--format', required=True, choices=["csv", "google_sheets"], help='Format of the master sheet.')
+    parser.add_argument('--logfile', help='File to log warnings and errors to.')
+    parser.add_argument('--config', help='JSON config file.')
+    args = parser.parse_args()
+
+    if args.logfile:
+        logging.basicConfig(filename=args.logfile, level=logging.WARNING, filemode='w')
+
+    if args.config is not None:
+        config = json.load(open(args.config, 'r'))
+    else:
+        config = {}
+
+    if args.format == 'csv':
+        sheet_parser = CSVMasterSheetParser(args.master_sheet)
+    else:
+        sheet_parser = GoogleMasterSheetParser(args.master_sheet)
+    flow_edit_sheets = sheet_parser.get_flow_edit_sheets(config)
+    rpx = RapidProABTestCreator(args.input)
     rpx.apply_abtests(flow_edit_sheets)
-    rpx.export_to_json(OUTPUT_FILENAME)
+    rpx.export_to_json(args.output)
+
+
 
 
 if __name__ == '__main__':
--- main_language_chooser.py
+++ main_language_chooser.py
@@ -2,29 +2,38 @@ from language_chooser import replace_flow_with_language_chooser
 from sheets import load_content_from_csv
 import json
 import logging
-
-logging.basicConfig(filename='main.log', level=logging.WARNING, filemode='w')
-
-
-INPUT_CSV = "testdata/language_choose_malaysia.csv"
-FLOW_NAME = "Minimal Flow"
-INPUT_JSON = "testdata/minimal.json"
-OUTPUT_JSON = "out.json"
+import argparse
 
 
 def main():
-    rpx = json.load(open(INPUT_JSON, 'r'))
-    flow = None
-    for flow_ in rpx["flows"]:
-        if flow_["name"] == FLOW_NAME:
-            flow = flow_
-    if flow is None:
-        logging.error(f"No flow of name {FLOW_NAME} exists.")
+    parser = argparse.ArgumentParser(description='Apply FlowEdits/ABTests to a RapidPro JSON file.')
+    parser.add_argument('input', help='RapidPro JSON file defining the input RapidPro flows.')
+    parser.add_argument('flow_name', help='Name of the flow to be replaced with a language chooser.')
+    parser.add_argument('output', help='RapidPro JSON file to write the output to.')
+    parser.add_argument('sheet', help='Sheet defining the language chooser options. Either a csv file or a Google Sheet ID.')
+    parser.add_argument('--format', required=True, choices=["csv", "google_sheets"], help='Format of the sheet.')
+    parser.add_argument('--logfile', help='File to log warnings and errors to.')
+    args = parser.parse_args()
+
+    if args.logfile:
+        logging.basicConfig(filename=args.logfile, level=logging.WARNING, filemode='w')
+    if args.format == 'csv':
+        data = load_content_from_csv(args.sheet)
+    else:
+        logging.error(f"Google Sheets currently not supported.")
         exit(0)
 
-    data = load_content_from_csv(INPUT_CSV)
+    rpx = json.load(open(args.input, 'r'))
+    flow = None
+    for flow_ in rpx["flows"]:
+        if flow_["name"] == args.flow_name:
+            flow = flow_
+    if flow is None:
+        logging.error(f"No flow of name {args.flow_name} exists.")
+        exit(0)
+
     replace_flow_with_language_chooser(data, flow)
-    json.dump(rpx, open(OUTPUT_JSON, 'w'), indent=2)
+    json.dump(rpx, open(args.output, 'w'), indent=2)
 
 
 if __name__ == '__main__':
