686c8f9a88569a6b52da8b8f6f19ed4b3f828638
Ian Stride <ianstride@gmail.com>
2023-07-05 19:03:17 +0100

    Extract code that applies A/B tests from 'main' function
    The intention is to be able to apply A/B tests to flows from other Python
    projects without using the CLI i.e. by importing the "apply" function.

--- src/rapidpro_abtesting/main.py
+++ src/rapidpro_abtesting/main.py
@@ -7,33 +7,81 @@ from .sheets import CSVMasterSheetParser, GoogleMasterSheetParser
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Apply FlowEdits/ABTests to a RapidPro JSON file.')
-    parser.add_argument('input', help='RapidPro JSON file defining the input RapidPro flows.')
-    parser.add_argument('output', help='RapidPro JSON file to write the output to.')
-    parser.add_argument('master_sheets', nargs='+', help='Master sheet(s) referencing FlowEdits/ABTests. Either csv file(s) or a Google Sheet ID(s).')
-    parser.add_argument('--format', required=True, choices=["csv", "google_sheets"], help='Format of the master sheet.')
-    parser.add_argument('--logfile', help='File to log warnings and errors to.')
-    parser.add_argument('--config', help='JSON config file.')
+    parser = argparse.ArgumentParser(
+        description="Apply FlowEdits/ABTests to a RapidPro JSON file."
+    )
+    parser.add_argument(
+        "input",
+        help="RapidPro JSON file defining the input RapidPro flows.",
+    )
+    parser.add_argument(
+        "output",
+        help="RapidPro JSON file to write the output to.",
+    )
+    parser.add_argument(
+        "master_sheets",
+        nargs="+",
+        help=(
+            "Master sheet(s) referencing FlowEdits/ABTests. "
+            "Either CSV file(s) or a Google Sheet ID(s)."
+        ),
+    )
+    parser.add_argument(
+        "--format",
+        required=True,
+        choices=["csv", "google_sheets"],
+        help="Format of the master sheet.",
+    )
+    parser.add_argument(
+        "--logfile",
+        help="File to log warnings and errors to.",
+    )
+    parser.add_argument(
+        "--config",
+        help="JSON config file.",
+    )
     args = parser.parse_args()
+    apply_abtests(
+        args.input,
+        args.output,
+        args.master_sheets,
+        args.format,
+        logfile=args.logfile,
+        config_fp=args.config,
+    )
 
-    if args.logfile:
-        logging.basicConfig(filename=args.logfile, level=logging.WARNING, filemode='w')
 
-    if args.config is not None:
-        config = json.load(open(args.config, 'r'))
+def apply_abtests(
+    input_flow,
+    output_flow,
+    main_sheets,
+    sheet_format,
+    logfile=None,
+    config_fp=None,
+):
+    if logfile:
+        logging.basicConfig(
+            filename=logfile,
+            level=logging.WARNING,
+            filemode="w",
+        )
+
+    config = {}
+    if config_fp:
+        with open(config_fp, "r") as config_file:
+            config = json.load(config_file)
+
+    if format == "csv":
+        sheet_parser = CSVMasterSheetParser(main_sheets)
     else:
-        config = {}
+        sheet_parser = GoogleMasterSheetParser(main_sheets)
 
-    if args.format == 'csv':
-        sheet_parser = CSVMasterSheetParser(args.master_sheets)
-    else:
-        sheet_parser = GoogleMasterSheetParser(args.master_sheets)
     flow_edit_sheet_groups = sheet_parser.get_flow_edit_sheet_groups(config)
-    rpx = RapidProABTestCreator(args.input)
+    rpx = RapidProABTestCreator(input_flow)
     for flow_edit_sheets in flow_edit_sheet_groups:
         rpx.apply_abtests(flow_edit_sheets)
-    rpx.export_to_json(args.output)
+    rpx.export_to_json(output_flow)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
